6c4ca23964d2c03c9743b59cdf03fd848a7c990f
lazyBit99 <alex.marchi99@gmail.com>
2020-03-04 18:39:57 +0100

    create infographic file

---
+++ src/components/infographic/Infographic.vue
@@ -0,0 +1,39 @@
+<template>
+	<div id="infographic">
+		<!-- <Map /> -->
+		<Header id="header" :bottleProp="bottleProp" />
+		<Default />
+	</div>
+</template>
+
+<script>
+import Map from './Map'
+import Header from './Header'
+import Default from './descriptions/Default'
+
+export default {
+	name: 'Infographic',
+	props: {
+		bottleProp: Number,
+	},
+	// data() {
+	// 	return {
+	// 		bottleData: this.bottleProp,
+	// 	}
+	// },
+	components: {
+		Map,
+		Header,
+		Default,
+	},
+}
+</script>
+
+<style>
+#infographic {
+	background-color: #2c2c2c;
+}
+#header {
+	margin-bottom: 20px;
+}
+</style>
