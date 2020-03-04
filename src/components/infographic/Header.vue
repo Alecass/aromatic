b9b96f57146b1fe222db0dcb08a6894280e9fee7
lazyBit99 <alex.marchi99@gmail.com>
2020-03-04 18:39:41 +0100

    create header file

---
+++ src/components/infographic/Header.vue
@@ -0,0 +1,39 @@
+<template>
+	<div id="header">
+		<h1 class="vino capitalize">
+			{{ values[bottleProp].vino }}
+		</h1>
+		<h3 class="produttore capitalize">
+			{{ values[bottleProp].produttore }}
+		</h3>
+	</div>
+</template>
+
+<script>
+import defaultViewValues from '../../../static/data/defaultViewValues.json'
+
+export default {
+	name: 'Header',
+	props: {
+		bottleProp: Number,
+	},
+	data() {
+		return {
+			// bottleData: this.bottleProp,
+			values: defaultViewValues,
+		}
+	},
+}
+</script>
+
+<style scoped>
+.produttore {
+	font-family: 'Playfair Display Bold';
+	font-style: italic;
+	color: #c2c2c2;
+}
+.vino {
+	font-family: 'Playfair Display Italic';
+	color: #b14d74;
+}
+</style>
