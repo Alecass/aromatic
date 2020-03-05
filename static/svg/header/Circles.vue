5c1575bc4b7d4988950b846d867c7f2e9481ffd6
lazyBit99 <alex.marchi99@gmail.com>
2020-03-05 12:36:13 +0100

    create svg path vue file

---
+++ static/svg/header/Circles.vue
@@ -0,0 +1,15 @@
+<template>
+	<g class="cls-1">
+		<path
+			class="cls-2"
+			d="M26.57,187.58a114.44,114.44,0,1,1,161,16.43A114.45,114.45,0,0,1,26.57,187.58Z"
+			transform="translate(0)"
+		/>
+	</g>
+</template>
+
+<script>
+export default {
+	name: 'Circles',
+}
+</script>
