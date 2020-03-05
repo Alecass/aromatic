f333cc30820388c0c52df548b665c397bf1c1643
lazyBit99 <alex.marchi99@gmail.com>
2020-03-05 12:36:54 +0100

    map WIP

--- src/components/infographic/Infographic.vue
+++ src/components/infographic/Infographic.vue
@@ -1,6 +1,6 @@
 <template>
 	<div id="infographic">
-		<!-- <Map /> -->
+		<Map />
 		<Header id="header" />
 		<Default />
 	</div>
--- src/components/infographic/Map.vue
+++ src/components/infographic/Map.vue
@@ -1,13 +1,20 @@
 <template>
 	<div id="map">
-		<p>map</p>
+		<WrapperSVG id="circles" title="circles" width="100" height="100"
+			><Circles
+		/></WrapperSVG>
 	</div>
 </template>
 
 <script>
+import WrapperSVG from '../../utils/WrapperSVG'
+import Circles from '../../../static/svg/header/Circles'
+
 export default {
 	name: 'Map',
+	components: {
+		WrapperSVG,
+		Circles,
+	},
 }
 </script>
-
-<style></style>
