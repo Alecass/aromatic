8abc8228a7c4a6838400235b4f21419beb4fea15
lazyBit99 <alex.marchi99@gmail.com>
2020-03-05 11:15:04 +0100

    update state manager WIP

--- src/assets/state.js
+++ src/assets/state.js
@@ -1,12 +1,34 @@
-let bottle = 0
+let BOTTLE = 0
 
-const state = {
+export const manager = {
 	setBottle: value => {
-		bottle = value
+		BOTTLE = value
 	},
 	getBottle: () => {
-		return bottle
+		return BOTTLE
 	},
 }
 
-export default state
+export const state = {
+	setBottle: () => {
+		window.addEventListener('keypress', e => {
+			let key = Number(e.key)
+
+			if (key >= 0 && key <= 6) manager.setBottle(key)
+			else return
+		})
+	},
+	getBottle: () => {
+		window.addEventListener('keypress', e => {
+			let key = Number(e.key)
+			let bottle = 0
+
+			if (key >= 0 && key <= 6) {
+				console.log('BOTTLE', manager.getBottle())
+				bottle = manager.getBottle()
+			}
+		})
+
+		return bottle
+	},
+}
