994df54afca5bebf6e3017a17c895f7efbe6c4de
lazyBit99 <alex.marchi99@gmail.com>
2020-03-06 17:36:48 +0100

    create widget component

---
+++ src/components/infographic/descriptions/Widget.vue
@@ -0,0 +1,31 @@
+<template>
+	<div id="widget">
+		<h1>Vino: {{ bottle }} - Bottone: {{ button }}</h1>
+	</div>
+</template>
+
+<script>
+import state from '../../../assets/state'
+
+export default {
+	name: 'Widget',
+	data() {
+		return {
+			bottle: state.watchBottle(),
+			button: state.watchButton(),
+		}
+	},
+	mounted() {
+		window.addEventListener('keypress', e => {
+			this.bottle = state.watchBottle()
+			this.button = state.watchButton()
+		})
+	},
+}
+</script>
+
+<style scoped>
+h1 {
+	color: white;
+}
+</style>
