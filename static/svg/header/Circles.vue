fc5b3c5957daa697be0d509c00bf7dba8f91d4de
Alessio <49764709+Alecass@users.noreply.github.com>
2020-03-06 17:35:11 +0100

    svg fix

--- static/svg/header/Circles.vue
+++ static/svg/header/Circles.vue
@@ -1,15 +1,28 @@
 <template>
-	<g class="cls-1">
-		<path
-			class="cls-2"
-			d="M26.57,187.58a114.44,114.44,0,1,1,161,16.43A114.45,114.45,0,0,1,26.57,187.58Z"
-			transform="translate(0)"
-		/>
-	</g>
+  <g class="cls-1">
+    <path
+      class="cls-2"
+      d="M26.57,187.58a114.44,114.44,0,1,1,161,16.43A114.45,114.45,0,0,1,26.57,187.58Z"
+      transform="translate(0)"
+    />
+  </g>
 </template>
 
 <script>
 export default {
-	name: 'Circles',
-}
+  name: "Circles"
+};
 </script>
+
+<style scoped>
+.cls-1 {
+  opacity: 0.6;
+}
+.cls-2 {
+  fill: none;
+  stroke: #fff;
+  stroke-miterlimit: 10;
+  stroke-width: 1.69px;
+  stroke-dasharray: 269.65 449.42;
+}
+</style>
